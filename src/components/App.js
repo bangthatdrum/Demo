4f7eb2bf8896e4942304544c42f5633e2e2e9343
thomas <thomashenryalderson@gmail.com>
2022-12-02 15:56:33 -0600

    15. push

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-import '../App.css'
 import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import {ethers} from 'ethers';
-import TOKEN1_ABI from '../abis/Token.json';
-import EXCHANGE_ABI from '../abis/Token.json';
 import config from '../config.json';
+import TOKEN1_ABI from '../abis/Token.json';
+
+import {loadProvider} from '../store/interactions';
 
 function App() {
+  const dispatch = useDispatch();
 
   async function loadBlockchainData() {
     const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
     console.log("Account address: ", accounts[0]);
 
     // Connect Ethers to blockchain
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = loadProvider(dispatch);
+
     const {chainId} = await provider.getNetwork();
     console.log("Chain Id: ", config[chainId].token1.address);
     // Token smart contract
